3c9170769e1cdb62c796de4aaef46238abbfb80c
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2024-11-16 17:38:47 +0000

    Initial commit

---
+++ atmateIntegration/src/main/java/com/atmate/portal/integration/atmateintegration/AtmateIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.atmate.portal.integration.atmateintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmateIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtmateIntegrationApplication.class, args);
+    }
+
+}
---
+++ atmateIntegration/src/test/java/com/atmate/portal/integration/atmateintegration/AtmateIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmate.portal.integration.atmateintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmateIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
