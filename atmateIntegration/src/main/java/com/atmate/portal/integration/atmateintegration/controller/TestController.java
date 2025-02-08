5616ec6a0e6c318ff6958e5030e549a9f3c8950e
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2025-02-08 13:16:31 +0000

    Create TestController.java

---
+++ atmateIntegration/src/main/java/com/atmate/portal/integration/atmateintegration/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.atmate.portal.integration.atmateintegration.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "A aplicação está a funcionar corretamente!";
+    }
+}
