31fedadf965bee0a9ba46db98396806b1853265a
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2025-02-08 13:30:47 +0000

    Create SecurityConfig.java

---
+++ atmateIntegration/src/main/java/com/atmate/portal/integration/atmateintegration/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.atmate.portal.integration.atmateintegration.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest().permitAll() // Permitir todas as requisições para teste
+                )
+                .csrf(csrf -> csrf.disable()); // Desabilitar CSRF apenas para testes
+
+        return http.build();
+    }
+}
+
+
