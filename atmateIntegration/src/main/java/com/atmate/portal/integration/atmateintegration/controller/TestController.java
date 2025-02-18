3e413f63b5c6ff39380000c54828084211bb636e
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2025-02-18 22:50:37 +0000

    Update TestController.java

--- atmateIntegration/src/main/java/com/atmate/portal/integration/atmateintegration/controller/TestController.java
+++ atmateIntegration/src/main/java/com/atmate/portal/integration/atmateintegration/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "A aplicação está a funcionar corretamente!";
+        return "A aplicação está a funcionar corretamente! Build Automático";
     }
 }
