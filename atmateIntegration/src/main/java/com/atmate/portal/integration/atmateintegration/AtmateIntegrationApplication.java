22b30a868dce5efeea9341c15637f5570d5d54cc
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2025-03-12 19:02:34 +0000

    Criado ficheiros para decriptaçã ode passwords

--- atmateIntegration/src/main/java/com/atmate/portal/integration/atmateintegration/AtmateIntegrationApplication.java
+++ atmateIntegration/src/main/java/com/atmate/portal/integration/atmateintegration/AtmateIntegrationApplication.java
@@ -2,8 +2,10 @@ package com.atmate.portal.integration.atmateintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AtmateIntegrationApplication {
 
     public static void main(String[] args) {
