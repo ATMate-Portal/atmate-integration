64810e2c0866466cc63ceaa3887e68a2693cd100
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2025-03-13 01:27:17 +0000

    Alterado para prototype para criar sempre um novo bean

--- atmateIntegration/src/main/java/com/atmate/portal/integration/atmateintegration/beans/threads/GetATDataThread.java
+++ atmateIntegration/src/main/java/com/atmate/portal/integration/atmateintegration/beans/threads/GetATDataThread.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
@@ -20,6 +21,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Optional;
 
+@Scope("prototype")
 @Component
 public class GetATDataThread implements Runnable {
 
