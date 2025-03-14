8c657913ce000f810d38abe47438b5a1334245ea
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2025-03-14 22:25:35 +0000

    Criar classes de criação de websocket

---
+++ atmateIntegration/src/main/java/com/atmate/portal/integration/atmateintegration/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.atmate.portal.integration.atmateintegration.config;
+
+import com.atmate.portal.integration.atmateintegration.handlers.LogWebSocketHandler;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final LogWebSocketHandler logWebSocketHandler;
+
+    @Value("${log.path}")
+    private String logPath;
+    public WebSocketConfig(LogWebSocketHandler logWebSocketHandler) {
+        this.logWebSocketHandler = logWebSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(logWebSocketHandler, logPath);
+    }
+}
