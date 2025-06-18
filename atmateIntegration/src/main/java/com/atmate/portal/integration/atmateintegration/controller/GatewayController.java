15c3c99ae831ea5e05a54f58063389fefaeb7be8
Tiago Oliveira <86232762+tiago0liveira77@users.noreply.github.com>
2025-06-18 11:40:35 +0100

    Completar info de swagger

--- atmateIntegration/src/main/java/com/atmate/portal/integration/atmateintegration/controller/GatewayController.java
+++ atmateIntegration/src/main/java/com/atmate/portal/integration/atmateintegration/controller/GatewayController.java
@@ -8,6 +8,13 @@ import com.atmate.portal.integration.atmateintegration.services.NotificationServ
 import com.atmate.portal.integration.atmateintegration.services.ScrappingService;
 import com.atmate.portal.integration.atmateintegration.utils.enums.ErrorEnum;
 import com.atmate.portal.integration.atmateintegration.utils.exceptions.ATMateException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("gateway")
+@Tag(name = "Gestão de clientes e notificações", description = "Endpoints para sincronização de dados AT e envio de notificações.")
 public class GatewayController {
     @Autowired
     ClientService clientService;
@@ -31,7 +39,42 @@ public class GatewayController {
     private static final Logger logger = LoggerFactory.getLogger(GatewayController.class);
 
     @GetMapping("/sync/{clientId}")
-    public ResponseEntity<?> sync(@PathVariable Integer clientId, @RequestParam(value = "getTypeFromAT", required = false, defaultValue = "false") boolean getTypeFromAT) throws Exception {
+    @Operation(
+            summary = "Atualizar dados AT de um cliente",
+            description = "Endpoint que invoca o web scraping à Autoridade Tributária (AT) para atualização completa dos dados de um cliente específico. Retorna 200 OK em caso de sucesso."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Dados do cliente sincronizados com sucesso.",
+                    content = @Content(schema = @Schema(implementation = Void.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Requisição inválida, por exemplo, cliente não encontrado.",
+                    content = @Content(schema = @Schema(
+                            type = "object",
+                            example = "{\n  \"timestamp\": \"2025-06-18T10:12:42.172+00:00\",\n  \"status\": 400,\n  \"error\": \"Bad Request\",\n  \"message\": \"O cliente não existe.\",\n  \"errorCode\": \"CLIENT-003\",\n  \"path\": \"/atmate-integration/gateway/sync/59\"\n}"
+                    ))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Erro interno do servidor ou falha durante o scraping.",
+                    content = @Content(schema = @Schema(implementation = String.class))
+            )
+    })
+    public ResponseEntity<?> sync(
+            @Parameter(
+                    description = "ID único do cliente a ser sincronizado.",
+                    required = true,
+                    example = "22"
+            )
+            @PathVariable Integer clientId,
+            @Parameter(
+                    description = "Define se o tipo de utilizador deve ser obtido da AT. Se 'true', o scraping tentará identificar o tipo de utilizador. Padrão é 'false'.",
+                    example = "true"
+            )
+            @RequestParam(value = "getTypeFromAT", required = false, defaultValue = "false") boolean getTypeFromAT) throws Exception {
         logger.info("Syncing client...");
         Client client = clientService.getClientById(clientId)
                 .orElseThrow(() -> new ATMateException(ErrorEnum.CLIENT_NOT_FOUND));
@@ -42,7 +85,34 @@ public class GatewayController {
     }
 
     @GetMapping("/sendNotification/{configId}")
-    public ResponseEntity<?> sendNotification(@PathVariable Integer configId) throws Exception {
+    @Operation(
+            summary = "Enviar notificações para uma configuração específica",
+            description = "Inicia o processo de preparação e execução de notificações com base numa configuração pré-existente. Retorna o número de notificações enviadas."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Notificações lançadas com sucesso. Retorna o número de notificações enviadas.",
+                    content = @Content(schema = @Schema(type = "integer", format = "int32", example = "5"))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Configuração de notificação não encontrada.",
+                    content = @Content(schema = @Schema(implementation = String.class))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Erro interno do servidor durante o envio das notificações.",
+                    content = @Content(schema = @Schema(implementation = String.class))
+            )
+    })
+    public ResponseEntity<?> sendNotification(
+            @Parameter(
+                    description = "ID único da configuração de notificação para a qual as notificações serão enviadas.",
+                    required = true,
+                    example = "408"
+            )
+            @PathVariable Integer configId) throws Exception {
         logger.info("Sending notification...");
 
         int numberOfSentNotification = notificationService.prepareAndTriggerSingleConfigNotifications(configId);
--- atmateIntegration/src/main/java/com/atmate/portal/integration/atmateintegration/controller/UtilController.java
+++ atmateIntegration/src/main/java/com/atmate/portal/integration/atmateintegration/controller/UtilController.java
@@ -4,6 +4,13 @@ import com.atmate.portal.integration.atmateintegration.services.EmailSendingServ
 import com.atmate.portal.integration.atmateintegration.services.NotificationService;
 import com.atmate.portal.integration.atmateintegration.services.SmsSendingService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/util")
+@Tag(name = "Utils e Testes", description = "Endpoints para testes de aplicação, envio de SMS e e-mails, e execução de configurações de notificação.")
 public class UtilController {
 
     private static final Logger logger = LoggerFactory.getLogger(UtilController.class);
@@ -31,6 +39,22 @@ public class UtilController {
     private NotificationService notificationService;
 
     @GetMapping("/health")
+    @Operation(
+            summary = "Verificação o estado da Aplicação",
+            description = "Verifica se a aplicação está em execução e a responder corretamente."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "A aplicação está a funcionar corretamente.",
+                    content = @Content(schema = @Schema(type = "string", example = "A aplicação está a funcionar corretamente!"))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Erro interno do servidor.",
+                    content = @Content(schema = @Schema(implementation = String.class))
+            )
+    })
     public ResponseEntity<String> healthCheck() {
         logger.info("Health check endpoint chamado.");
         return ResponseEntity.ok("A aplicação está a funcionar corretamente!");
@@ -43,34 +67,71 @@ public class UtilController {
      * @return Uma resposta indicando o sucesso ou falha (simulado).
      */
     @PostMapping("/send-sms")
+    @Operation(
+            summary = "Envio de SMS",
+            description = "Envia um SMS para um número de telefone especificado."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "SMS enviado com sucesso.",
+                    content = @Content(schema = @Schema(type = "string", example = "SMS enviado para 912345678"))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Falha interna ao tentar enviar o SMS ou erro na comunicação com o serviço de SMS.",
+                    content = @Content(schema = @Schema(type = "string", example = "Erro interno ao tentar enviar SMS: Erro na API do SMS"))
+            )
+    })
     public ResponseEntity<String> testSendSms(
+            @Parameter(
+                    description = "O número de telefone do destinatário do SMS (com código de país, ex: '351912345678').",
+                    required = true,
+                    example = "351912345678"
+            )
             @RequestParam String phoneNumber,
+            @Parameter(
+                    description = "O conteúdo da mensagem SMS a ser enviada.",
+                    required = true,
+                    example = "Olá, este é um SMS de teste da aplicação!"
+            )
             @RequestParam String message) {
-        logger.info("Tentativa de envio de SMS de teste para: {} com mensagem: '{}'", phoneNumber, message);
+        logger.info("Tentativa de envio de SMS para: {} com mensagem: '{}'", phoneNumber, message);
 
         try {
-             boolean smsSent = smsSendingService.sendSms(phoneNumber, message);
-             if (smsSent) {
-                 logger.info("SMS de teste enviado com sucesso para {}", phoneNumber);
-                 return ResponseEntity.ok("SMS de teste enviado (simulado) para " + phoneNumber);
-             } else {
-                 logger.error("Falha ao enviar SMS de teste para {}", phoneNumber);
-                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Falha ao enviar SMS de teste (simulado)");
-             }
+            boolean smsSent = smsSendingService.sendSms(phoneNumber, message);
+            if (smsSent) {
+                logger.info("SMS enviado com sucesso para {}", phoneNumber);
+                return ResponseEntity.ok("SMS para " + phoneNumber);
+            } else {
+                logger.error("Falha ao enviar SMS para {}", phoneNumber);
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Falha ao enviar SMS");
+            }
 
         } catch (Exception e) {
-            logger.error("Erro ao tentar enviar SMS de teste para {}: {}", phoneNumber, e.getMessage(), e);
+            logger.error("Erro ao tentar enviar SMS para {}: {}", phoneNumber, e.getMessage(), e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Erro interno ao tentar enviar SMS de teste (simulado): " + e.getMessage());
+                    .body("Erro interno ao tentar enviar SMS: " + e.getMessage());
         }
     }
 
-    /**
-     * Endpoint de teste para simular o envio de um SMS.
-
-     * @return Uma resposta indicando o sucesso ou falha (simulado).
-     */
     @GetMapping("/runNotificationConfig")
+    @Operation(
+            summary = "Preparar Configuração de Notificações",
+            description = "Invoca preparação das notificações baseadas nas configurações existentes na base de dados."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Processamento das notificações iniciado com sucesso.",
+                    content = @Content(schema = @Schema(implementation = Void.class)) // Retorna void
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Erro interno do servidor durante o processamento das notificações.",
+                    content = @Content(schema = @Schema(implementation = String.class, example = "Erro interno ao processar notificações."))
+            )
+    })
     public void runNotificationConfig() throws JsonProcessingException {
         notificationService.prepareNotifications();
     }
@@ -83,22 +144,53 @@ public class UtilController {
      * @return Uma resposta indicando o sucesso ou falha (simulado).
      */
     @PostMapping("/send-email")
+    @Operation(
+            summary = "Envio de Email",
+            description = "Envia um e-mail para um endereço especificado."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Email enviado com sucesso (simulado).",
+                    content = @Content(schema = @Schema(type = "string", example = "Email enviado para teste@example.com"))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Falha interna ao tentar enviar o email ou erro na comunicação com o serviço de e-mail.",
+                    content = @Content(schema = @Schema(type = "string", example = "Erro interno ao tentar enviar email: Erro na conexão SMTP"))
+            )
+    })
     public ResponseEntity<String> testSendEmail(
+            @Parameter(
+                    description = "O endereço de e-mail do destinatário.",
+                    required = true,
+                    example = "teste@example.com"
+            )
             @RequestParam String toEmail,
+            @Parameter(
+                    description = "O assunto do e-mail.",
+                    required = true,
+                    example = "Assunto do Teste de Email"
+            )
             @RequestParam String subject,
+            @Parameter(
+                    description = "O corpo do e-mail.",
+                    required = true,
+                    example = "Este é o corpo da mensagem de email."
+            )
             @RequestParam String body) {
-        logger.info("Tentativa de envio de Email de teste para: {} com assunto: '{}'", toEmail, subject);
+        logger.info("Tentativa de envio de Email para: {} com assunto: '{}'", toEmail, subject);
 
 
         try {
             emailSendingService.sendEmail(toEmail, subject, body, true);
-            logger.info("Email de teste enviado com sucesso para {}", toEmail);
-            return ResponseEntity.ok("Email de teste enviado (simulado) para " + toEmail);
+            logger.info("Email enviado com sucesso para {}", toEmail);
+            return ResponseEntity.ok("Email enviado (simulado) para " + toEmail);
 
         } catch (Exception e) {
-            logger.error("Erro ao tentar enviar email de teste para {}: {}", toEmail, e.getMessage(), e);
+            logger.error("Erro ao tentar enviar email para {}: {}", toEmail, e.getMessage(), e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Erro interno ao tentar enviar email de teste (simulado): " + e.getMessage());
+                    .body("Erro interno ao tentar enviar email: " + e.getMessage());
         }
     }
 }
